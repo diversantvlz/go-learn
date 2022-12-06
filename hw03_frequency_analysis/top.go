81922d5098e8dc24aaba3dd7cf85039dfe6f9030
SilyutinVV <vyacheslav.silyutin@vseinstrumenti.ru>
2022-12-06 20:17:13 +0300

    hw3 completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -13,11 +13,28 @@ func Top10(inputString string) []string {
 	words := strings.Fields(strings.ToLower(inputString))
 	counts := map[string]int{}
 
+	if len(words) == 0 {
+		return words
+	}
+
 	for _, value := range words {
+		if "-" == value {
+			continue
+		}
+
 		counts[value]++
 	}
-	sort.SliceStable(counts, func(i, j int) bool {
-		return i < j
+
+	keys := make([]string, 0, len(counts))
+
+	for key := range counts {
+		keys = append(keys, key)
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		return (counts[keys[i]] > counts[keys[j]]) ||
+			(counts[keys[i]] == counts[keys[j]] && keys[i] < keys[j])
 	})
-	return nil
+
+	return keys[0:10]
 }
