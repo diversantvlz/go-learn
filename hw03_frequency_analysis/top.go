83c526571ccff6070f9c1c95e3ff3d56e5d858b0
SilyutinVV <vyacheslav.silyutin@vseinstrumenti.ru>
2022-12-06 18:41:43 +0300

    prepare string

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,23 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var dotsRe = regexp.MustCompile(`[,.!?'"]`)
+
+func Top10(inputString string) []string {
+	inputString = dotsRe.ReplaceAllString(inputString, "")
+	words := strings.Fields(strings.ToLower(inputString))
+	counts := map[string]int{}
+
+	for _, value := range words {
+		counts[value]++
+	}
+	sort.SliceStable(counts, func(i, j int) bool {
+		return i < j
+	})
 	return nil
 }
