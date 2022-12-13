4934344bb044045d53809ec7f22fa17e75c31851
SilyutinVV <vyacheslav.silyutin@vseinstrumenti.ru>
2022-12-13 13:22:00 +0300

    init

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,66 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+type Queue struct {
+	sync.RWMutex
+	tasks []Task
+	index int
+}
+
+func (q *Queue) get() Task {
+	q.RLock()
+	defer q.RUnlock()
+
+	if q.index == len(q.tasks) {
+		return nil
+	}
+
+	task := q.tasks[q.index]
+	q.index++
+
+	return task
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	wg := &sync.WaitGroup{}
+	wg.Add(n)
+	queue := Queue{tasks: tasks}
+
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			for true {
+				if 0 >= m {
+					break
+				}
+
+				task := queue.get()
+
+				if nil == task {
+					break
+				}
+
+				result := task()
+				if nil != result {
+					m--
+				}
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	if 0 >= m {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
