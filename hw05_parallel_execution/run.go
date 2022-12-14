64b694ad107024047a638ed29cddccbdc0b740c1
SilyutinVV <vyacheslav.silyutin@vseinstrumenti.ru>
2022-12-14 20:56:50 +0300

    fix CR

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,53 +10,39 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-type Queue struct {
-	sync.RWMutex
-	tasks []Task
-	index int
-}
-
-func (q *Queue) get() Task {
-	q.Lock()
-	defer q.Unlock()
-
-	if q.index == len(q.tasks) {
-		return nil
-	}
-
-	task := q.tasks[q.index]
-	q.index++
-
-	return task
-}
-
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	wg := &sync.WaitGroup{}
-	queue := Queue{tasks: tasks}
+	queue := make(chan Task, len(tasks))
+	for _, task := range tasks {
+		queue <- task
+	}
+
 	var errCnt int32
 	wg.Add(n)
 	for i := 0; i < n; i++ {
 		go func(wg *sync.WaitGroup) {
 			defer wg.Done()
+		out:
 			for {
 				if atomic.LoadInt32(&errCnt) >= int32(m) {
 					break
 				}
 
-				task := queue.get()
-				if nil == task {
-					break
-				}
-
-				if task() != nil {
-					atomic.AddInt32(&errCnt, 1)
+				select {
+				case task := <-queue:
+					if task() != nil {
+						atomic.AddInt32(&errCnt, 1)
+					}
+				default:
+					break out
 				}
 			}
 		}(wg)
 	}
 
 	wg.Wait()
+	close(queue)
 
 	if errCnt >= int32(m) {
 		return ErrErrorsLimitExceeded
