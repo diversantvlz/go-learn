9caf75c6b581f358e029f262fb24f0492e627251
SilyutinVV <vyacheslav.silyutin@vseinstrumenti.ru>
2022-12-15 11:45:20 +0300

    fix CR

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -12,37 +12,33 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	wg := &sync.WaitGroup{}
-	queue := make(chan Task, len(tasks))
-	for _, task := range tasks {
-		queue <- task
-	}
-
 	var errCnt int32
-	wg.Add(n)
+	wg := &sync.WaitGroup{}
+	wg.Add(n + 1)
+	queue := make(chan Task)
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		for _, task := range tasks {
+			if atomic.LoadInt32(&errCnt) >= int32(m) {
+				break
+			}
+			queue <- task
+		}
+		close(queue)
+	}(wg)
+
 	for i := 0; i < n; i++ {
 		go func(wg *sync.WaitGroup) {
 			defer wg.Done()
-		out:
-			for {
-				if atomic.LoadInt32(&errCnt) >= int32(m) {
-					break
-				}
-
-				select {
-				case task := <-queue:
-					if task() != nil {
-						atomic.AddInt32(&errCnt, 1)
-					}
-				default:
-					break out
+			for task := range queue {
+				if task() != nil {
+					atomic.AddInt32(&errCnt, 1)
 				}
 			}
 		}(wg)
 	}
 
 	wg.Wait()
-	close(queue)
 
 	if errCnt >= int32(m) {
 		return ErrErrorsLimitExceeded
