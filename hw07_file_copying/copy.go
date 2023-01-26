f3b17f511b9e5254b70f4eedb302c27029cf2c10
SilyutinVV <vyacheslav.silyutin@vseinstrumenti.ru>
2023-01-26 04:46:07 +0300

    copy logic

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
 )
 
 var (
@@ -9,7 +13,63 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+type ProgressBar struct {
+	total    int64
+	progress int64
+}
+
+func (pb *ProgressBar) Start(total int64) {
+	pb.total = total
+	pb.progress = 0
+
+	pb.print()
+}
+
+func (pb *ProgressBar) Advance(value int64) {
+	pb.progress += value
+	pb.print()
+}
+
+func (pb *ProgressBar) print() {
+	percent := int64((float32(pb.progress) / float32(pb.total)) * 100)
+	fmt.Printf("\r[%-100s]%3d%% %8d/%d", strings.Repeat("=", int(percent)), percent, pb.progress, pb.total)
+}
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+	pb := ProgressBar{}
+	src, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+
+	stat, _ := src.Stat()
+
+	if stat.Size() == 0 || stat.IsDir() {
+		return ErrUnsupportedFile
+	}
+
+	if offset > stat.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	maxLimit := stat.Size() - offset
+	if limit == 0 || limit > maxLimit {
+		limit = maxLimit
+	}
+
+	pb.Start(limit)
+	dst, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	_, err = src.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+
+	result, err := io.CopyN(dst, src, limit)
+	pb.Advance(result)
+
+	return err
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,86 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	outputFile := "testdata/output.txt"
+
+	t.Run("copy full", func(t *testing.T) {
+		err := Copy("testdata/input.txt", outputFile, 0, 0)
+
+		expected, _ := os.ReadFile("testdata/out_offset0_limit0.txt")
+		actual, _ := os.ReadFile(outputFile)
+		require.NoError(t, err)
+		require.Equal(t, expected, actual)
+		_ = os.Remove(outputFile)
+	})
+
+	t.Run("copy 0:10", func(t *testing.T) {
+		err := Copy("testdata/input.txt", outputFile, 0, 10)
+
+		expected, _ := os.ReadFile("testdata/out_offset0_limit10.txt")
+		actual, _ := os.ReadFile(outputFile)
+		require.NoError(t, err)
+		require.Equal(t, expected, actual)
+		_ = os.Remove(outputFile)
+	})
+
+	t.Run("copy 0:1000", func(t *testing.T) {
+		err := Copy("testdata/input.txt", outputFile, 0, 1000)
+
+		expected, _ := os.ReadFile("testdata/out_offset0_limit1000.txt")
+		actual, _ := os.ReadFile(outputFile)
+		require.NoError(t, err)
+		require.Equal(t, expected, actual)
+		_ = os.Remove(outputFile)
+	})
+
+	t.Run("copy 0:10000", func(t *testing.T) {
+		err := Copy("testdata/input.txt", outputFile, 0, 10000)
+
+		expected, _ := os.ReadFile("testdata/out_offset0_limit10000.txt")
+		actual, _ := os.ReadFile(outputFile)
+		require.NoError(t, err)
+		require.Equal(t, expected, actual)
+		_ = os.Remove(outputFile)
+	})
+
+	t.Run("copy 100:1000", func(t *testing.T) {
+		err := Copy("testdata/input.txt", outputFile, 100, 1000)
+
+		expected, _ := os.ReadFile("testdata/out_offset100_limit1000.txt")
+		actual, _ := os.ReadFile(outputFile)
+		require.NoError(t, err)
+		require.Equal(t, expected, actual)
+		_ = os.Remove(outputFile)
+	})
+
+	t.Run("copy 6000:1000", func(t *testing.T) {
+		err := Copy("testdata/input.txt", outputFile, 6000, 1000)
+
+		expected, _ := os.ReadFile("testdata/out_offset6000_limit1000.txt")
+		actual, _ := os.ReadFile(outputFile)
+		require.NoError(t, err)
+		require.Equal(t, expected, actual)
+		_ = os.Remove(outputFile)
+	})
+
+	t.Run("copy 10000:1000", func(t *testing.T) {
+		err := Copy("testdata/input.txt", outputFile, 10000, 1000)
+		require.Error(t, err)
+	})
+
+	t.Run("copy no length", func(t *testing.T) {
+		err := Copy("/dev/urandom", outputFile, 0, 0)
+		require.Error(t, err)
+	})
+
+	t.Run("copy dir", func(t *testing.T) {
+		err := Copy("testdata", outputFile, 0, 0)
+		require.Error(t, err)
+	})
 }
